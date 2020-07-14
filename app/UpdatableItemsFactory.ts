195e5ba39e6998119d9658b91755104c04567175
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-07-14 23:10:53 +0200

    Solidifcation: Sulfuras items

--- app/UpdatableItemsFactory.ts
+++ app/UpdatableItemsFactory.ts
@@ -1,9 +1,11 @@
 import AgedBrieItem from './AgedBrieItem';
 import GeneralItem from './GeneralItem';
 import { Item } from './gilded-rose';
+import SulfurasItem from './SulfurasItem';
 import UpdatableItem from './UpdatableItem';
 
 const AgedBrie = 'Aged Brie';
+const Sulfuras = 'Sulfuras, Hand of Ragnaros';
 
 export default class UpdatableItemsFactory {
   static fromItemsArray(items: Array<Item>): Array<UpdatableItem> {
@@ -14,6 +16,9 @@ export default class UpdatableItemsFactory {
     switch (item.name) {
       case AgedBrie:
         return new AgedBrieItem(item.name, item.sellIn, item.quality);
+
+      case Sulfuras:
+        return new SulfurasItem(item.name, item.sellIn, item.quality);
 
       default:
         return new GeneralItem(item.name, item.sellIn, item.quality);
